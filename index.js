b528dad0c01936781479983bdcf407500ae101f3
lokeshpandey1407 <luvpnd@gmail.com>
2024-08-22 01:13:17 +0530

    dotenv

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Auth = require("./src/middleware/auth.middleware");
 const adminRoutes = require("./src/features/admin/admin.routes");
 
 const app = express();
-
+require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
